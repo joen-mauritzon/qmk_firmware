324e666a0b5dfdf89c3b16e63574bb7173b38dde
Joen Mauritzon <joenmauritzon@MacBook-Pro.local>
2016-05-08 08:36:05 +0200

    Update layout look more like HHBK
    Change layout to look more like HHKB
    Change SYMB layer digits to numpad digits

--- keyboard/ergodox_ez/keymaps/my_layout/keymap.c
+++ keyboard/ergodox_ez/keymaps/my_layout/keymap.c
@@ -34,50 +34,50 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /**
 * Layer: BASE
-* /-------//-------//-------//-------//-------//-------//-------/            /-------//-------//-------//-------//-------//-------//-------/           
-* | =      | 1      | 2      | 3      | 4      | 5      | LEFT   |           | RGHT   | 6      | 7      | 8      | 9      | 0      | -      |          
-* |        |        |        |        |        |        |        |           |        |        |        |        |        |        |        |          
-* /-------//-------//-------//-------//-------//-------//-------/            /-------//-------//-------//-------//-------//-------//-------/           
-* | TAB    | Q      | W      | E      | R      | T      | 1      |           | 1      | Y      | U      | I      | O      | P      | \      |          
-* |        |        |        |        |        |        |        |           |        |        |        |        |        |        |        |          
-* /-------//-------//-------//-------//-------//-------/\-------\            \-------\/-------//-------//-------//-------//-------//-------/           
-* | ESC    | A      | S      | D      | F      | G      |                             | H      | J      | K      | L      | ;      | '      |          
-* | Ctrl   |        |        |        |        |        |                             |        |        |        |        |        |        |          
-* /-------//-------//-------//-------//-------//-------//-------/            /-------//-------//-------//-------//-------//-------//-------/           
-* | Shift  | Z      | X      | C      | V      | B      | NO     |           | NO     | N      | M      | ,      | .      | /      | Shift  |          
-* |        |        |        |        |        |        | Hyper  |           | Meh    |        |        |        |        | Ctrl   |        |          
-* \-------\\-------\\-------\\-------\\-------\\-------\\-------\            \-------\\-------\\-------\\-------\\-------\\-------\\-------\           
-*                                                                                                                                                      
-*                                                                                                                                                      
-*                   /-------//-------//-------//-------//-------/                                       /-------//-------//-------//-------//-------/  
-*                   | `      | '      | LEFT   | RIGHT  | Alt    |                                      | MDIA   | DOWN   | [      | ]      | MDIA   | 
-*                   | SYMB   |        |        |        |        |                                      |        |        |        |        |        | 
-*                   \-------\\-------\\-------\\-------\/-------//-------/   /-------//-------/         \-------\\-------\\-------\\-------\\-------\  
-*                                                       | APP    | Cmd    |  | Alt    | ESC    |                                                       
-*                                                       | ALT    |        |  |        | Ctrl   |                                                       
-*                                                       \-------\/-------/   /-------/\-------\                                                        
-*                                                                | HOME   |  | PGUP   |                                                                
-*                                                                |        |  |        |                                                                
-*                                              /-------//-------//-------/   /-------//-------//-------/                                               
-*                                              | SPC    | BSPC   | CMD+END|  | PGDN   | Cmd    | ENT    |                                              
-*                                              |        |        |        |  |        |        | MDIA   |                                              
-*                                              \-------\\-------\\-------\   \-------\\-------\\-------\                                               
-*                                                                                                                                                      
+* /-----//-----//-----//-----//-----//-----//-----/          /-----//-----//-----//-----//-----//-----//-----/         
+* | =    | 1    | 2    | 3    | 4    | 5    | 6    |         | 7    | 8    | 9    | 0    | -    | \    | `    |        
+* |      |      |      |      |      |      |      |         |      |      |      |      |      |      |      |        
+* /-----//-----//-----//-----//-----//-----//-----/          /-----//-----//-----//-----//-----//-----//-----/         
+* | TAB  | Q    | W    | E    | R    | T    | 1    |         | 1    | Y    | U    | I    | O    | P    | [    |        
+* |      |      |      |      |      |      |      |         |      |      |      |      |      |      |      |        
+* /-----//-----//-----//-----//-----//-----/\-----\          \-----\/-----//-----//-----//-----//-----//-----/         
+* | ESC  | A    | S    | D    | F    | G    |                       | H    | J    | K    | L    | ;    | '    |        
+* | Ctrl |      |      |      |      |      |                       |      |      |      |      |      |      |        
+* /-----//-----//-----//-----//-----//-----//-----/          /-----//-----//-----//-----//-----//-----//-----/         
+* | Shift| Z    | X    | C    | V    | B    | NO   |         | NO   | N    | M    | ,    | .    | /    | Shift|        
+* |      |      |      |      |      |      | Hyper|         | Meh  |      |      |      |      | Ctrl |      |        
+* \-----\\-----\\-----\\-----\\-----\\-----\\-----\          \-----\\-----\\-----\\-----\\-----\\-----\\-----\         
+*                                                                                                                      
+*                                                                                                                      
+*               /-----//-----//-----//-----//-----/                               /-----//-----//-----//-----//-----/  
+*               | `    | '    | LEFT | RIGHT| Alt  |                              | MDIA | DOWN | [    | ]    | MDIA | 
+*               | SYMB |      |      |      |      |                              |      |      |      |      |      | 
+*               \-----\\-----\\-----\\-----\/-----//-----/   /-----//-----/       \-----\\-----\\-----\\-----\\-----\  
+*                                           | APP  | Cmd  |  | Alt  | ESC  |                                           
+*                                           | ALT  |      |  |      | Ctrl |                                           
+*                                           \-----\/-----/   /-----/\-----\                                            
+*                                                  | HOME |  | PGUP |                                                  
+*                                                  |      |  |      |                                                  
+*                                    /-----//-----//-----/   /-----//-----//-----/                                     
+*                                    | SPC  | BSPC | END  |  | PGDN | Cmd  | ENT  |                                    
+*                                    |      |      | CMD  |  |      |      | MDIA |                                    
+*                                    \-----\\-----\\-----\   \-----\\-----\\-----\                                     
+*                                                                                                                      
 * 
 **/
 [BASE]=KEYMAP(
 //left half
-    KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_LEFT, 
+    KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, 
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, TG(1), 
     CTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, 
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, ALL_T(KC_NO), 
                   LT(SYMB,KC_GRV), KC_QUOT, KC_LEFT, KC_RIGHT, KC_LALT, 
                                               ALT_T(KC_APP), KC_LGUI, 
                                                      KC_HOME, 
-                                       KC_SPC, KC_BSPC, LGUI(KC_END), 
+                                       KC_SPC, KC_BSPC, GUI_T(KC_END), 
     //right half
-    KC_RGHT, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, 
-    TG(1), KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, 
+    KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_BSLS, KC_GRV, 
+    TG(1), KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, 
            KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
     MEH_T(KC_NO), KC_N, KC_M, KC_COMM, KC_DOT, CTL_T(KC_SLSH), KC_RSFT, 
                          MO(MDIA), KC_DOWN, KC_LBRC, KC_RBRC, MO(MDIA), 
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 *                                                                                                      
 *                                                                                                      
 *             /----//----//----//----//----/                           /----//----//----//----//----/  
-*             |     |     |     |     |     |                          |     | .   | 0   | =   |     | 
+*             |     |     |     |     |     |                          | 0   | E   | 0   | =   |     | 
 *             |     |     |     |     |     |                          |     |     |     |     |     | 
 *             \----\\----\\----\\----\/----//----/   /----//----/      \----\\----\\----\\----\\----\  
 *                                     |     |     |  |     |     |                                     
@@ -129,17 +129,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        KC_TRNS, KC_TRNS, KC_TRNS, 
     //right half
     KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, 
-    KC_TRNS, KC_UP, KC_7, KC_8, KC_9, KC_ASTR, KC_F12, 
-           KC_DOWN, KC_4, KC_5, KC_6, KC_PLUS, KC_TRNS, 
-    KC_TRNS, KC_AMPR, KC_1, KC_2, KC_3, KC_BSLS, KC_TRNS, 
-                         KC_TRNS, KC_DOT, KC_0, KC_EQL, KC_TRNS, 
+    KC_TRNS, KC_UP, KC_KP_7, KC_KP_8, KC_KP_9, KC_ASTR, KC_F12, 
+           KC_DOWN, KC_KP_4, KC_KP_5, KC_KP_6, KC_PLUS, KC_TRNS, 
+    KC_TRNS, KC_AMPR, KC_KP_1, KC_KP_2, KC_KP_3, KC_BSLS, KC_TRNS, 
+                         KC_KP_0, KC_E, KC_0, KC_EQL, KC_TRNS, 
     KC_TRNS, KC_TRNS, 
     KC_TRNS, 
     KC_TRNS, KC_TRNS, KC_TRNS),
 /**
 * Layer: MDIA
 * /-----//-----//-----//-----//-----//-----//-----/          /-----//-----//-----//-----//-----//-----//-----/         
-* |      |      |      |      |      |      |      |         |      |      |      |      |      |      |      |        
+* |      |      |      |      |      |      |      |         |      |      |      |      |      | SLEEP| POWER|        
 * |      |      |      |      |      |      |      |         |      |      |      |      |      |      |      |        
 * /-----//-----//-----//-----//-----//-----//-----/          /-----//-----//-----//-----//-----//-----//-----/         
 * |      |      |      | U    |      |      |      |         |      |      |      |      |      | RIGHT|      |        
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |      |      | L    | D    | R    |      |                       |      | LEFT |      |      |      |      |        
 * |      |      |      |      |      |      |                       |      |      |      |      |      |      |        
 * /-----//-----//-----//-----//-----//-----//-----/          /-----//-----//-----//-----//-----//-----//-----/         
-* |      |      |      |      |      |      |      |         |      |      | VOLD | VOLU | MUTE | MPLY |      |        
+* |      |      |      | DOWN | UP   |      |      |         |      |      | VOLD | VOLU | MUTE | MPLY |      |        
 * |      |      |      |      |      |      |      |         |      |      |      |      |      |      |      |        
 * \-----\\-----\\-----\\-----\\-----\\-----\\-----\          \-----\\-----\\-----\\-----\\-----\\-----\\-----\         
 *                                                                                                                      
@@ -174,13 +174,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
     KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS, 
     KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN, KC_UP, KC_TRNS, KC_TRNS, 
                   KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2, 
                                               KC_TRNS, KC_TRNS, 
                                                      KC_TRNS, 
                                        KC_TRNS, KC_TRNS, KC_TRNS, 
     //right half
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SYSTEM_SLEEP, KC_POWER, 
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RIGHT, KC_TRNS, 
            KC_TRNS, KC_LEFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
     KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPLY, KC_TRNS, 
@@ -207,12 +207,15 @@ case 0:
 
 
 
+
+
         if (record->event.pressed) {
           register_code(KC_RSFT);
         } else {
           unregister_code(KC_RSFT);
         }
         
+
 
 
 
